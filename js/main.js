77079ac9e595cd843ad278798c3ef695dda997f5
Carolina Knoll <carolinasknoll@gmail.com>
2016-05-13 16:49:36 -0300

    Add latest-works section with carousel

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:40,
+  nav:true,
+  navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+  autoplay:true,
+  autoplayTimeout:3000,
+  autoplayHoverPause:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      400:{
+          items:2
+      },
+      768:{
+          items:4
+      }
+  }
+});
